fc615a041345186e508c475edf08f6df491aaa87
matsvei-kavalenka <matsvei.kavalenka@gmail.com>
2024-10-25 21:08:45 +0300

    Added different prints, grouped function

--- main.py
+++ main.py
@@ -9,16 +9,11 @@ def main():
 
     while True:
         print("\nMenu:")
-        print("1. Print Transactions")
-        print("2. Print Total Spending")
-        print("3. Add a New Transaction")
+        print("1. Add a New Transaction")
+        print("2. Print Transactions")
+        print("3. Print Total Spending")
         print("4. Update Transaction")
         print("5. Delete a Transaction")
-        '''
-        filter from the smallest to the biggest and backwards
-        print all transactions by category(groceries, date, type)
-        
-        '''
         print("\n0. Exit\n")
         print('Enter a Number: ', end='')
 
@@ -26,30 +21,6 @@ def main():
 
         match value:
             case 1:
-                while True:
-                    print("\nChoose Print Option:")
-                    print("1. Print All Transactions")
-                    print("2. Print Filtered By Price Transactions")
-                    print("3. Print Custom Filtered Transactions")
-
-                    print("\n0. Go Back\n")
-                    print('Enter a Number: ', end='')
-                    option = input()
-
-                    match option:
-                        case 1:
-                            # Case 1 - Print All Transactions
-                            output = spendwise.get_all_transactions()
-                            if not output:
-                                print('\n\n-------------------------')
-                                print("There Are No Transactions")
-                                print('-------------------------\n')
-                            else:
-                                print(output)
-            case 2:
-                total_price = spendwise.print_total_spending()
-                print(total_price)
-            case 3:
                 print("\nEnter Transaction Description: ", end="")
                 description = input()
 
@@ -62,6 +33,44 @@ def main():
                 print("Enter Transaction Type: ", end="")
                 trans_type = input()
                 spendwise.add_transaction(description, date, price, trans_type)
+            case 2:
+                while True:
+                    print("\nChoose Print Option:")
+                    print("1. Print All Transactions")
+                    print("2. Print Ordering  By Transactions")
+
+                    print("\n0. Go Back\n")
+                    print('Enter a Number: ', end='')
+                    option = int(input())
+
+                    match option:
+                        case 1:
+                            # Case 1 - Print All Transactions
+                            output = spendwise.get_all_transactions()
+                            if not output:
+                                spendwise.print_output('There Are No Transactions')
+                            else:
+                                spendwise.print_output(str(output))
+                            break
+                        case 2:
+                            # 2 - Print Custom Sorted Transactions
+                            print("\nWrite Category To Order By: ", end="")
+                            category = input()
+
+                            print("\nWrite Ascending Or Descending Order(ASC or DESC): ", end="")
+                            order = input()
+
+                            output = spendwise.get_custom_ordered_transactions(category, order)
+                            spendwise.print_output(str(output))
+                            break
+                        case 0:
+                            break
+                        case _:
+                            print('Invalid Input. Try again.')
+            case 3:
+                total_price = spendwise.get_total_spending()
+                print(total_price)
+
             case 4:
                 print("\nEnter ID of Transaction To Update: ", end="")
                 trans_id = int(input())
--- spendwise.py
+++ spendwise.py
@@ -1,3 +1,6 @@
+import shutil
+
+
 class SpendWise:
     def __init__(self, db, cursor):
         self.cursor = cursor
@@ -18,29 +21,46 @@ class SpendWise:
 
         return results
 
-    def print_total_spending(self) -> int:
+    def get_custom_ordered_transactions(self, category: str, order: str) -> list[tuple]:
+        """
+                A function that prints all transactions from the database
+
+                :param category: The category of the transaction.
+                :param order: The order of the transaction.
+                :returns: List of tuples.
+                :example:
+                >>> self.get_custom_ordered_transactions('price', 'DESC')
+                "[(5, '2024-10-11', 'Shopping', 43.89, 'Clothes'),
+                (4, '2024-10-08', 'Netflix Subscription', 7.99, 'Subscription'),
+                (1, '2024-10-02', 'Spotify Payment', 4.99, 'Subscription')]"
+                """
+        query = f"SELECT * FROM Spending ORDER BY {category} {order}"
+        self.cursor.execute(query)
+        results = self.cursor.fetchall()
+
+        return results
+
+    def get_total_spending(self) -> float:
         """
             A function that calculates the total spending by summing the 'price' column from all transactions.
 
             :returns: Total spending as an integer. If no transactions exist, returns 0.
-
             :example:
-            >>> self.print_total_spending()
+            >>> self.get_total_spending()
             120
             """
         self.cursor.execute("SELECT SUM(price) as Total_Spending FROM Spending")
         result = self.cursor.fetchone()
-        return result[0] if result[0] is not None else 0
+        return round(result[0], 2) if result[0] is not None else 0
 
-    def add_transaction(self, description, date, price, transaction_type):
+    def add_transaction(self, description, date, price, transaction_type) -> None:
         """
             A function that adds a new transaction to the database.
 
-            :param description: Description of the transaction (e.g., 'Spotify Payment').
-            :param date: Date of the transaction (e.g., '2024-10-02').
-            :param price: Price of the transaction (e.g., 4.99).
-            :param transaction_type: Type of transaction (e.g., 'Subscription', 'Groceries').
-
+            :param description: Description of the transaction.
+            :param date: Date of the transaction.
+            :param price: Price of the transaction.
+            :param transaction_type: Type of transaction.
             :example:
             >>> self.add_transaction('Spotify Payment', '2024-10-02', 4.99, 'Subscription')
             Transaction Added Successfully
@@ -52,17 +72,14 @@ class SpendWise:
         except Exception as e:
             print(e)
         finally:
-            print('\n\n-------------------------')
-            print('Transaction Added Successfully')
-            print('-------------------------\n')
+            self.print_output('Transaction Added Successfully')
 
-    def update_transaction(self, transaction_id: int, transaction_data: str):
+    def update_transaction(self, transaction_id: int, transaction_data: str) -> None:
         """
             A function that updates an existing transaction in the database.
 
             :param transaction_id: ID of the transaction to be updated.
             :param transaction_data: A string containing the fields to be updated and their new values.
-
             :example:
             >>> self.update_transaction(1, '"description": "Updated Description", "price": 10.99')
             Transaction Updated Successfully
@@ -70,31 +87,39 @@ class SpendWise:
         try:
             self.cursor.execute(f"UPDATE Spending SET {transaction_data} WHERE id = {transaction_id}")
             self.db.commit()
-            print('\n\n-------------------------')
-            print('Transaction Updated Successfully')
-            print('-------------------------\n')
+            self.print_output('Transaction Updated Successfully')
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    def delete_transaction(self, transaction_id: int):
+    def delete_transaction(self, transaction_id: int) -> None:
         """
-            A function that deletes a transaction from the database.
+        A function that deletes a transaction from the database.
 
-            :param transaction_id: ID of the transaction to be deleted.
-
-            :example:
-            >>> self.delete_transaction(1)
-            Transaction Deleted Successfully
-            """
+        :param transaction_id: ID of the transaction to be deleted.
+        :example:
+        >>> self.delete_transaction(1)
+        Transaction Deleted Successfully
+        """
         try:
             self.cursor.execute('DELETE FROM Spending WHERE id = ?', (transaction_id,))
             self.db.commit()
-            print('\n\n-------------------------')
-            print('Transaction Deleted Successfully')
-            print('-------------------------\n')
+            self.print_output('Transaction Deleted Successfully')
         except Exception as e:
             print(f"An error occurred: {e}")
 
+    def print_output(self, output: str) -> None:
+        """
+        A function that deletes a transaction from the database.
 
+        :param output: Output of the transaction.
+        :example:
+        >>> self.print_output('Transaction Deleted Successfully')
+        -------------------------
+        Transaction Deleted Successfully
+        -------------------------
+        """
+        width = shutil.get_terminal_size().columns
 
-
+        print('\n\n', '-' * width, sep='')
+        print(output)
+        print('-' * width, '\n')
